98123125c9f03f0d2140bf0e52b304e8c2f61e95
Birger Johansson <m@birgerjohansson.com>
2025-09-04 14:16:17 +0200

    Adding a catch all to redirect user to /

--- epiguess/src/App.js
+++ epiguess/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-import { Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import robot_image from './images/robot_illu3.png';
 import './App.css';
 import EmotionObject from './EmotionObject';
@@ -29,6 +28,8 @@ import { zeros } from '@tensorflow/tfjs-core';
 //emotionDisplay will store the emotion category acting as basis for the interaction
 //counter only necessary if DB
 //currentState will keep track on the view/state
+
+
 
 class App extends React.Component {
   constructor(props) {
@@ -302,6 +303,9 @@ class App extends React.Component {
               {/* <Route path='/PrevResult' component={prevResultElem}/> */}
               <Route path='/Result' component={resultElem} />
               <Route path='/Summary' component={summaryElem} />
+              <Route path="*">
+    <Redirect to="/" />
+  </Route>
             </Switch>
           </div>
         </div>
